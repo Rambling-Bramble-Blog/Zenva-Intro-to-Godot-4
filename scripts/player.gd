5d1d66da1e504511ec6014934a63d42fe7f38be4
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-14 19:23:07 -0800

    H: Coin collector started. Player's input-based movement was created in the player script. Lesson 51

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var speed : float = 100
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	velocity.x = 0
+	velocity.y = 0
+	
+	# movement based on directional key input
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += speed
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= speed
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= speed
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += speed
+		
+	move_and_slide()
